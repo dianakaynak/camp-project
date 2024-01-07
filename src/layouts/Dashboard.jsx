69f7c410955c4913c524437ad30015189ffcef73
dianakaynak <dianaonder90@gmail.com>
2024-01-08 01:57:31 +0300

    Verification processes have been added

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom'
 import ProductDetail from '../pages/ProductDetail'
 import CartDetail from '../pages/CartDetail'
 import { ToastContainer } from 'react-toastify'
+import ProductAdd from '../pages/ProductAdd'
 
 
 export default function Dashboard() {
@@ -23,6 +24,8 @@ export default function Dashboard() {
               <Route path="/products" Component={ProductList} />
               <Route exact path="/products/:id" Component={ProductDetail} />
               <Route exact path="/cart" Component={CartDetail} />
+              <Route exact path="/product/add" Component={ProductAdd} />
+
             </Routes>
           </Grid.Column>
         </Grid.Row>
